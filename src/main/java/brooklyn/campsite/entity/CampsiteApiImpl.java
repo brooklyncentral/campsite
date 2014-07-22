e30490a8742568084358c810acd62a2f6928fb9c
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-07-22 15:52:48 +0100

    Bugfixes

--- src/main/java/brooklyn/campsite/entity/CampsiteApiImpl.java
+++ src/main/java/brooklyn/campsite/entity/CampsiteApiImpl.java
@@ -1,10 +1,17 @@
 package brooklyn.campsite.entity;
 
+import brooklyn.entity.basic.Attributes;
 import brooklyn.entity.basic.Entities;
 import brooklyn.entity.webapp.nodejs.NodeJsWebAppServiceImpl;
+import brooklyn.location.basic.PortRanges;
 import brooklyn.util.time.Duration;
 
 public class CampsiteApiImpl extends NodeJsWebAppServiceImpl implements CampsiteApi {
+
+    @Override
+    public void init() {
+        setConfig(Attributes.HTTP_PORT, PortRanges.fromInteger(getConfig(API_PORT)));
+    }
 
     @Override
     public Class<?> getDriverInterface() {
